37a5cdf2414af6a3fa79ad5a32a5d885bf5ba029
solarslurpi <solarslurpi@users.noreply.github.com>
2024-09-05 10:51:21 -0700

    evolved how the misting process went. Simplified

--- src/service/mistbuddy_manager.py
+++ src/service/mistbuddy_manager.py
@@ -10,43 +10,41 @@ logger = logging.getLogger(__name__)
 
 class MistBuddyManager:
     def __init__(self):
-        self.stop_event: asyncio.Event | None = None
-        self.timer_task: asyncio.Task | None = None
-        self.power_instance: PowerBuddy | None = None
 
     async def start_mistbuddy(self, host_ip: str, tent_name: str, duration_on: float):
         await self.stop_mistbuddy()  # Stop any running misting, just in case
         try:
             self.power_instance = PowerBuddy(tent_name, host_ip)
+            self.mist(True, duration_on=5)
             # Start the timer to run turn_on_power_task every 60 seconds
-            self.stop_event = asyncio.Event()
-            self.timer_task = asyncio.create_task(self.power_instance.async_timer(60, self.stop_event, duration_on))
-        except ValueError:
+
+        except ValueError as exc:
             logger.error("Value error in start_mistbuddy", exc_info=True)
-            raise HTTPException(status_code=400, detail="Invalid input parameters")
-        except asyncio.CancelledError:
+            raise HTTPException(status_code=400, detail="Invalid input parameters") from exc
+        except asyncio.CancelledError as exc:
             logger.info("Misting operation cancelled")
             await self.stop_mistbuddy()
-        except Exception:
+            raise HTTPException(status_code=400, detail='Invalid input parameters') from exc
+
+        except Exception as exc:
             logger.error("Unexpected error in start_mistbuddy", exc_info=True)
             await self.stop_mistbuddy()
-            raise HTTPException(status_code=500, detail="Error starting MistBuddy")
-        logger.info(f"MistBuddy started successfully.  Will spew mist every {duration_on} seconds each minute.")
+            raise HTTPException(status_code=500, detail="Error starting MistBuddy") from exc
+        logger.info("MistBuddy started successfully.  Will spew mist every %s seconds each minute.", duration_on)
+
+    def mist(self, on: bool, duration_on):
+        try:
+            while on is True and self.power_instance is not None:
+                self.power_instance.power_on(duration_on)
+                asyncio.sleep()
+        except asyncio.CancelledError:
+            logger.info("Misting operation cancelled")
+            raise HTTPException(status_code=400, detail='Misting operation cancelled')
 
     async def stop_mistbuddy(self):
-        if self.timer_task:
-            self.timer_task.cancel()
-            try:
-                await self.timer_task
-            except asyncio.CancelledError:
-                pass
-
-        if self.stop_event:
-            self.stop_event.set()
-
-        self.timer_task = None
+        self.mist(False)
         self.power_instance = None
-        self.stop_event = None
+
         logger.info("MistBuddy stopped successfully.")
 
 _mistbuddy_manager = None
--- src/service/power_code.py
+++ src/service/power_code.py
@@ -9,27 +9,19 @@ from src.service.mqtt_publish_code import MQTTClient
 logger = logging.getLogger(__name__)
 
 class PowerBuddy:
-    def __init__(self, tent_name:str, host_ip: str):
+    def __init__(self, tent_name: str, host_ip: str):
+        self.power_topics = config.get_power_topics(tent_name, "MistBuddy")
+        if not self.power_topics:
+            raise ValueError(f"No power topics returned for tent_name: {tent_name}")
         try:
-            self.power_topics = []
-            self.mqtt = None
-            self.power_topics = config.get_power_topics(tent_name,"MistBuddy")
-            if not self.power_topics or len(self.power_topics) == 0:
-                logger.error(f"No power topics returned for tent_name: {tent_name}")
-                raise ValueError(f"No power topics returned for tent_name: {tent_name}")
-            try:
-                self.mqtt = MQTTClient(host=host_ip)
-                logger.debug(f"MQTT client created for host: {host_ip}")
-            except Exception as e:
-                logger.error(f"Error setting up MQTT client", exc_info=True)
-                raise
-        except AttributeError as e:
-            logger.error(f"Error attempting to get the host_ip and power_topics from settings.  Error: {e}")
-            raise e
+            self.mqtt = MQTTClient(host=host_ip)
+            logger.debug(f"MQTT client created for host: {host_ip}")
+        except Exception as e:
+            logger.error(f"Error setting up MQTT client", exc_info=True)
+            raise RuntimeError("Error setting up MQTT client") from e
 
-    async def power_on(self, seconds_on: float) -> None:
+    def power_on(self, seconds_on: float) -> None:
         for power_command in self.power_topics:
-            # Define pulsetime_command before the try block
             parts = power_command.split("/")
             parts[-1] = "PulseTime"
             pulsetime_command = "/".join(parts)
@@ -41,10 +33,7 @@ class PowerBuddy:
 
                 # We will use numbers between 112 and 64900 for the PulseTime (e.g.: 112 - 100 = 12 seconds)
                 pulsetime_on = seconds_on + 100
-                logger.debug(
-                    f"PulseTime: {pulsetime_on} Number seconds on: {pulsetime_on - 100}"
-                )
-                # LET TASMOTA KNOW HOW LONG TO KEEP THE POWER ON.
+                logger.debug(f"PulseTime: {pulsetime_on} Number seconds on: {pulsetime_on - 100}")
                 self.mqtt.publish(pulsetime_command, pulsetime_on, qos=1)
             except Exception as e:
                 error_message = (
@@ -53,9 +42,3 @@ class PowerBuddy:
                 )
                 logger.error(error_message)
                 raise RuntimeError(error_message)
-
-
-    async def async_timer(self,interval, stop_event,  seconds_on ):
-        while not stop_event.is_set():
-            await self.power_on( seconds_on )
-            await asyncio.sleep(interval)
